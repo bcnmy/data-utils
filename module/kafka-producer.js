7278530a287d5ac8e1c9bc8cfbe4dfd5f991a773
Charan Teja Kattumenu <charanteja.kattumenu@gmail.com>
2023-09-18 18:00:28 +0530

    Checking if schema already exists for topic

--- module/kafka-producer.js
+++ module/kafka-producer.js
@@ -10,20 +10,31 @@ class KafkaProducer {
     });
     this.schemaRegistry = new SchemaRegistry({ host: config.schemaRegistryUrl });
     this.producer = this.kafka.producer();
-    this.schemaId = 0
   }
 
   async pushData(topic, schema, data) {
-    const options = {
-        subject: `${topic}-value`
+    const subject = `${topic}-value`;
+    var schemaId = undefined;
+    // check if schema exists for this topic already
+    try {
+      console.log(`Schema found for topic ${topic}. Not registering schema again`);
+      schemaId = await this.schemaRegistry.getLatestSchemaId(subject);
+    } catch(error) {
+      console.log(`Schema not found for topic ${topic}`);
     }
-    const { id } = await this.schemaRegistry.register({
+    if (schemaId === undefined) {
+      const options = {
+        subject: subject
+      } 
+      const { id } = await this.schemaRegistry.register({
         type: SchemaType.AVRO,
         schema,
-    }, options);
+      }, options);
+      schemaId = id;
+    }
     await this.producer.connect();
     const outgoingMessage = {
-      value: await this.schemaRegistry.encode(id, data)
+      value: await this.schemaRegistry.encode(schemaId, data)
     }
     await this.producer.send({
       topic: topic,
